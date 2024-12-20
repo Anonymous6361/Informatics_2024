a0a46524fb5b10eacdf865ce95afccc66c9a5032
Puhov Ivan <godfromsky2006@mail.ru>
2024-12-20 08:23:34 +0000

    lab7

--- golang/main.go
+++ golang/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"isuct.ru/informatics2022/laba6"
 	"isuct.ru/informatics2022/laba4"
+	"isuct.ru/informatics2022/lab7"
 )
 
 func main() {
 	laba4.CompleteLaba4()
 	laba6.CompleteLaba6()
+	lab7.CompleteLab7()
 }
